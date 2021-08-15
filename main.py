7375d0f0a8444b52877897c496acf2d22f48a3ac
binity <49219986+binity@users.noreply.github.com>
2021-08-15 20:47:51 +0800

    first commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.deterministic = True
 
-console.
+console.log('lz')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--action', default='train')
